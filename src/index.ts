a8a3d97029c41d4b5f26687121d31120d4189191
Luiz Otavio <luizomf@gmail.com>
2020-07-04 02:41:11 -0300

    Vantagens e desvantagens

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+Vantagens e desvantagens do S.O.L.I.D.
+
+Vantagens:
+- Código modular
+- Código reutilizável (D.R.Y - Don't repeat yourself)
+- Código testável, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Código expansível
+- Separations of concerns (Separação de conceitos)
+- Fácil manutenção
+Desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta
+- Tempo de desenvolvimento aumenta bastante
+- Tenha cuidados com: YAGNI, KISS (You aren't gonna need it, Keep it simple, stupid!)
+*/
